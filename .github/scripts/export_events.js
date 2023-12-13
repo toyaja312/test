b064be9a73a6fffea6a13cae4ea12d4c00f192ca
Toyaja M <63727789+toyaja312@users.noreply.github.com>
2023-12-13 11:10:55 +0530

    Update export_events.js

--- .github/scripts/export_events.js
+++ .github/scripts/export_events.js
@@ -1,35 +1,44 @@
-const axios = require('axios');
-
-async function fetchGitHubIssuesData(owner, repo, issueNumber) {
-  const apiUrl = `https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/timeline`;
-  const response = await axios.get(apiUrl);
-  return response.data;
+function fetchGitHubIssueData(owner, repo, issueNumber) {
+  var apiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issueNumber;
+  var response = UrlFetchApp.fetch(apiUrl);
+  return JSON.parse(response.getContentText());
 }
 
-async function updateGoogleSheet(sheetId, apiKey, data) {
-  const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
-
-  const values = data.map(item => Object.values(item));
-  const range = 'A1';  // Update with the desired range
-
-  const response = await axios.put(apiUrl, { values, range });
-  return response.data;
+function fetchGitHubIssueTimeline(owner, repo, issueNumber) {
+  var apiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issueNumber + '/timeline';
+  var response = UrlFetchApp.fetch(apiUrl);
+  return JSON.parse(response.getContentText());
 }
 
-async function run() {
-  const owner = process.env.OWNER;
-  const repo = process.env.REPO;
-  const issueNumber = process.env.ISSUE_NUMBER;
-  const sheetId = process.env.SHEET_ID;
-  const apiKey = process.env.GOOGLE_API_KEY;
+function updateGoogleSheet(sheetId, apiKey, issueData, issueTimeline) {
+  // Assuming 'title' is available in issueData and 'event' is available in issueTimeline
+  var values = [[issueData.title, issueTimeline.event]];
+  var range = 'Sheet1!A1:B1';  // Update with the desired range
 
-  const issuesData = await fetchGitHubIssuesData(owner, repo, issueNumber);
-  const updateResult = await updateGoogleSheet(sheetId, apiKey, issuesData);
+  var apiUrl = 'https://sheets.googleapis.com/v4/spreadsheets/' + sheetId + '/values/' + range + '?key=' + apiKey;
 
-  console.log('Data imported successfully:', updateResult);
+  var params = {
+    method: 'put',
+    contentType: 'application/json',
+    payload: JSON.stringify({ values: values }),
+    muteHttpExceptions: true
+  };
+
+  var response = UrlFetchApp.fetch(apiUrl, params);
+  return JSON.parse(response.getContentText());
 }
 
-run().catch(error => {
-  console.error('Error importing data:', error);
-  process.exit(1);
-});
+function run() {
+  var owner = process.env.OWNER;  // You need to set up Environment Variables in Google Apps Script
+  var repo = process.env.REPO;
+  var issueNumber = process.env.ISSUE_NUMBER;
+  var sheetId = process.env.SHEET_ID;
+  var apiKey = process.env.GOOGLE_API_KEY;
+
+  var issueData = fetchGitHubIssueData(owner, repo, issueNumber);
+  var issueTimeline = fetchGitHubIssueTimeline(owner, repo, issueNumber);
+
+  var updateResult = updateGoogleSheet(sheetId, apiKey, issueData, issueTimeline);
+
+  Logger.log('Data imported successfully:', updateResult);
+}
