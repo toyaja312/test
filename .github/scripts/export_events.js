1c38c27783037d404d42e6e62230bf55d59e3a88
Toyaja M <63727789+toyaja312@users.noreply.github.com>
2023-12-18 14:02:34 +0530

    Update export_events.js

--- .github/scripts/export_events.js
+++ .github/scripts/export_events.js
@@ -2,51 +2,44 @@ function importGitHubIssuesData() {
   // Replace these with your own values
   var owner = 'Dartle-Sports';
   var repo = 'slt-academy-web';
-  var issueNumber = 'ISSUE_NUMBER';
   var accessToken = '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'; // Replace with your GitHub access token
 
-  // Fetch issue data
-  var issueApiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issueNumber;
-  var issueResponse = UrlFetchApp.fetch(issueApiUrl, {
+  // Fetch all issues (both open and closed)
+  var issuesApiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues?state=all';
+  var issuesResponse = UrlFetchApp.fetch(issuesApiUrl, {
     headers: {
       Authorization: 'Bearer ' + accessToken
     }
   });
-  var issueData = JSON.parse(issueResponse.getContentText());
+  var issuesData = JSON.parse(issuesResponse.getContentText());
 
-  // Fetch timeline data
-  var timelineApiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issueNumber + '/timeline';
-  var timelineResponse = UrlFetchApp.fetch(timelineApiUrl, {
-    headers: {
-      Authorization: 'Bearer ' + accessToken
-    }
-  });
-  var timelineData = JSON.parse(timelineResponse.getContentText());
+  // Get the active sheet
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
 
-  // Create a new Google Sheet
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().insertSheet('GitHub Issues Data');
+  // Clear the contents of the existing sheet
+  sheet.clearContents();
 
-  // Write issue headers
-  var issueHeaders = Object.keys(issueData);
-  sheet.getRange(1, 1, 1, issueHeaders.length).setValues([issueHeaders]);
+  // Write headers for issue and timeline data
+  var issueHeaders = ['Issue Number'];
+  var timelineHeaders = ['Event', 'Commit ID', 'Label', 'Created At'];
+  sheet.getRange(1, 1, 1, issueHeaders.length + timelineHeaders.length).setValues([issueHeaders.concat(timelineHeaders)]);
 
-  // Write issue data
-  var issueValues = [issueHeaders.map(function (header) {
-    return issueData[header];
-  })];
-  sheet.getRange(2, 1, 1, issueHeaders.length).setValues(issueValues);
+  var rowIndex = 2; // Start from the second row to leave space for headers
 
-  // Write timeline headers
-  var timelineHeaders = Object.keys(timelineData[0]);
-  sheet.getRange(4, 1, 1, timelineHeaders.length).setValues([timelineHeaders]);
+  // Iterate through each issue
+  for (var i = 0; i < issuesData.length; i++) {
+    var issue = issuesData[i];
+    var issueNumber = issue.number;
 
-  // Write timeline data
-  var timelineValues = timelineData.map(function (item) {
-    return timelineHeaders.map(function (header) {
-      return item[header];
+    // Fetch timeline data for the current issue
+    var timelineApiUrl = 'https://api.github.com/repos/' + owner + '/' + repo + '/issues/' + issueNumber + '/timeline';
+    var timelineResponse = UrlFetchApp.fetch(timelineApiUrl, {
+      headers: {
+        Authorization: 'Bearer ' + accessToken
+      }
     });
-  });
-  sheet.getRange(5, 1, timelineValues.length, timelineHeaders.length).setValues(timelineValues);
+    var timelineData = JSON.parse(timelineResponse.getContentText());
 
-  Logger.log('Data imported successfully.');
-}
+    // Iterate through each event in the timeline data
+    for (var j = 0; j < timelineData.length; j++) {
+      var timelineEvent = timelineD
